d8c5f332f2feda20d6d504c77ce76d7995dd26bc
Piotr Dybowski <piotr.dybowski@codepoets.it>
2018-06-21 11:03:40 +0200

    Patch sleeper decorator in unit tests for prime (good)

--- test_primes.py
+++ test_primes.py
@@ -1,18 +1,16 @@
 from unittest import TestCase
 from unittest.mock import patch
-
+patch('primes.sleeper').start()
 from primes import is_prime
 
 
 class TestIsPrime(TestCase):
-    @patch('primes.sleeper')
-    def test_tahat_seven_is_prime(self, sleeper_mock):
+    def test_tahat_seven_is_prime(self):
         result = is_prime(7)
 
         self.assertEqual(result, True)
 
-    @patch('primes.sleeper')
-    def test_that_four_is_not_prime(self, sleeper_mock):
+    def test_that_four_is_not_prime(self):
         result = is_prime(4)
 
         self.assertEqual(result, False)
