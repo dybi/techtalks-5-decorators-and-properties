34c8ec4cd777462633995c1932d518c8a768c40b
Piotr Dybowski <piotr.dybowski@codepoets.it>
2018-06-21 06:57:47 +0200

    Add execution_counter decorator

--- decorators.py
+++ decorators.py
@@ -9,3 +9,13 @@ def execution_timer(fun):
         print(f"Execution time: {(stop-start).microseconds} microseconds")
         return result
     return inner
+
+
+def execution_counter(fun):
+    def wrapper(*args, **kwargs):
+        result = fun(*args, **kwargs)
+        wrapper.counter += 1
+        print(f"\"{fun.__name__}\" has been called {wrapper.counter} times")
+        return result
+    wrapper.counter = 0
+    return wrapper
