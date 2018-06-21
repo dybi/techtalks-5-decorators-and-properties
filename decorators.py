f9cec08ffafca6d96d39f22f199d5e7e0e2f243d
Piotr Dybowski <piotr.dybowski@codepoets.it>
2018-06-21 06:49:33 +0200

    Make execution_timer returns a value

--- decorators.py
+++ decorators.py
@@ -4,7 +4,8 @@ from datetime import datetime
 def execution_timer(fun):
     def inner(*args, **kwargs):
         start = datetime.now()
-        fun(*args, **kwargs)
+        result = fun(*args, **kwargs)
         stop = datetime.now()
         print(f"Execution time: {(stop-start).microseconds} microseconds")
+        return result
     return inner
